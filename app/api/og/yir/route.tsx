ed8a16f069add91b527a5eca8685876be50b2393
wta2001 <57104055+wta2001@users.noreply.github.com>
2025-12-12 12:48:55 -0500

    Change runtime from nodejs to edge

--- app/api/og/yir/route.tsx
+++ app/api/og/yir/route.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
 
-export const runtime = "nodejs";
+export const runtime = "edge";
 
 export async function GET(req: NextRequest) {
   const inter = await fetch(
